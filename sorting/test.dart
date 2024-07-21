1d1a891c7a1993bf3d37ba893f2bc006ca8ae263
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-07-21 13:08:00 -0300

    test: add random_list to sorting test

--- sorting/test.dart
+++ sorting/test.dart
@@ -8,29 +8,35 @@ void main() {
     test('Bubble Sort Algorithm', () {
       var ordernated_list = [1, 2, 3, 4, 5];
       var inverse_list = [5, 4, 3, 2, 1];
+      var random_list = [5, 2, 3, 1, 4];
       var repeated_list = [4, 4, 3, 3, 5, 5, 1, 1, 2, 2];
 
       bubbleSort(ordernated_list);
       bubbleSort(inverse_list);
+      bubbleSort(random_list);
       bubbleSort(repeated_list);
 
       expect(ordernated_list, [1, 2, 3, 4, 5]);
       expect(inverse_list, [1, 2, 3, 4, 5]);
       expect(repeated_list, [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
+      expect(random_list, [1, 2, 3, 4, 5]);
     });
 
     test('Insertion Sort Algorithm', () {
       var ordernated_list = [1, 2, 3, 4, 5];
       var inverse_list = [5, 4, 3, 2, 1];
+      var random_list = [5, 2, 3, 1, 4];
       var repeated_list = [4, 4, 3, 3, 5, 5, 1, 1, 2, 2];
 
       insertionSort(ordernated_list);
       insertionSort(inverse_list);
       insertionSort(repeated_list);
+      insertionSort(random_list);
 
       expect(ordernated_list, [1, 2, 3, 4, 5]);
       expect(inverse_list, [1, 2, 3, 4, 5]);
       expect(repeated_list, [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]);
+      expect(random_list, [1, 2, 3, 4, 5]);
     });
   });
 }
